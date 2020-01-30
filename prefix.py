e51d5389c96833368271fae590c70c81c211ec71
KAA-sign <q89308033587@gmail.com>
2020-01-30 17:45:38 +0300

    one more task

--- prefix.py
+++ prefix.py
@@ -1,11 +1,11 @@
 s = input()
 i = 0
 cnt = 1
-while i < len(s):
+while i < (len(s) - 1):
     if s[i] == s[i+1]:
         cnt += 1
-    elif s[i] != s[i+1] or i < (len(s) - 1):
-        print(s[i], cnt, end='')
+    elif s[i] != s[i+1]:
+        print(s[i] + cnt, end='')
         cnt = 1
     i += 1
-print()
+print(s[i] + cnt, end='')
