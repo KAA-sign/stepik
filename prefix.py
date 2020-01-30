22c6f82e3b9b56dd507d9ce34f294d212febd541
KAA-sign <q89308033587@gmail.com>
2020-01-30 19:34:24 +0300

    nothing

--- prefix.py
+++ prefix.py
@@ -4,6 +4,8 @@ cnt = 1
 while i < len(s):
     if s[i] == s[i+1]:
         cnt += 1
-    else:
-        print(i, count, end='')
+    elif s[i] != s[i+1] or i < (len(s) - 1):
+        print(s[i], cnt, end='')
+        cnt = 1
+    i += 1
 print()
