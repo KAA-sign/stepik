d548a447690ea6a7c11dd58aa982d431a982c6b0
KAA-sign <q89308033587@gmail.com>
2020-02-03 19:26:47 +0300

    2_task_1

---
+++ 2_task_1.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+result = [int(i) for i in range(1, n + 1)]
+#count = 1
+#result = []
+#while count <= n and len(result) <= n:
+#    value = str(count) * count
+#    result.extend([value])
+#    count += 1
+#result = [str(i) for i in result]
+#result = ''.join(result)
+#print(' '.join(result[0:n]))
+print(result)
