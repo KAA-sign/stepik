2cb90d2f59dbdfc81580ac97f538b41d17f4825e
KAA-sign <q89308033587@gmail.com>
2020-03-25 20:58:32 +0300

    +

--- function.py
+++ function.py
@@ -1,4 +1,10 @@
-def f(n):
-    return n * 10 + 5
+def f(x):
+    if x <= -2:
+        return 1 - ((x + 2) ** 2)
+    elif -2 < x <= 2:
+        return -x / 2
+    else:
+        return ((x -2) ** 2) + 1
+        
+        
     
-f(f(f(10)))
