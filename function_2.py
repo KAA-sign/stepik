188b1198d3504f8c5cd5714b766159ea54bee6b7
KAA-sign <q89308033587@gmail.com>
2020-03-28 14:20:37 +0300

    +

--- function_2.py
+++ function_2.py
@@ -1,10 +1,11 @@
-
-
 def modify_list(lst):
-    new_list = []
-    for i in lst:
+    new_lst = []
+    for i in  reversed(lst):
         if  i % 2 != 0: 
-            lst.remove(i) 
-            lst.append(i // 2)
-
-    modify_list([1, 2, 3, 4, 5, 6])
+            lst.remove(i)
+    for i in lst:
+        new_lst.append(i // 2)
+    lst[:] = new_lst
+    
+    
+modify_list([1, 2, 3, 4, 5, 6])
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-lst = [1, 2, 3, 4, 5, 6]
-for i in lst:
+lst = [1, 3, 5, 7]
+new_lst = []
+for i in reversed(lst):
     if  i % 2 != 0: 
-        lst.remove(i) 
-        for i in lst:
-            lst.append(i // 2)
+        lst.remove(i)
+for i in lst:
+    new_lst.append(i // 2)
+lst[:] = new_lst
