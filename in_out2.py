c55d85e112672854e7ea13e5e4433082b8f065ea
KAA-sign <q89308033587@gmail.com>
2020-04-15 08:14:40 +0300

    +

--- in_out2.py
+++ in_out2.py
@@ -1,5 +1,6 @@
 with open('/home/anthony/projects/stepik/in.txt') as f:
     lst = []
+    first_sum, second_sum, third_sum, count = 0, 0, 0, 0
     for line in f:
         name, first, second, third = line.split(';')
         lst.append(line.strip().split(';'))
@@ -7,26 +8,8 @@ with open('/home/anthony/projects/stepik/in.txt') as f:
         first_sum += int(first)
         second_sum += int(second)
         third_sum += int(third)
+        count += 1
         print(result)
-    # summ = 0
-    # i = 1
-    # print(lst)
-    # for s in lst: 
-    #     for mid in s:
-    #         mid = s[i]
-    #      summ += int(mid)
-    #  print(summ)
-    #     i += 1 
-    # print(summ)
-
-# line = line.split()
-#         if line and line[1][0].isdigit():
-#             interface, address, *other = line
-#             result[interface] = address
-# result = 0
-# # for line in f:
-# name, first, second, third = line.split(';')
-# result = (int(first) + int(second) + int(third)) / 3
-# print(result)
+    print((first_sum / count), (second_sum / count), (third_sum / count))
 
 
