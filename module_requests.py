abc057f873e2ed3279bcc25e9636464e07e41f22
KAA-sign <q89308033587@gmail.com>
2020-09-15 11:29:46 +0300

    +

---
+++ module_requests.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.get('https://stepic.org/media/attachments/course67/3.6.2/662.txt')
+
+
+data = response.text
+
+count = 0
+for line in data.splitlines():
+    count += 1
+print(count)
+    
