17df65f3e7473dceb0ef3a89b7c27c731d365f76
KAA-sign <q89308033587@gmail.com>
2020-09-17 16:33:19 +0300

    +

--- module_request_2.py
+++ module_request_2.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
-
 import requests
 
 first_response = requests.get('https://stepic.org/media/attachments/course67/3.6.3/699991.txt')
-
 data = str(first_response.text)
-print(data)
 while not data.startswith('We'):
     next_response = requests.get('https://stepic.org/media/attachments/course67/3.6.3/'+data)
+    data = str(next_response.text)
+    print(data)
 
 
 # count = 0
