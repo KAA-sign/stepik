87526c79211b1c263f22049c68310b5ac0e2b0a1
KAA-sign <q89308033587@gmail.com>
2020-10-01 16:31:52 +0300

    +

--- decoder.py
+++ decoder.py
@@ -26,30 +26,36 @@ dacabac
 
 '''
 
-# original_alphabet = list(input('Enter original alphadet: '))
-original_alphabet = list('abcd')
-# finite_alphabet = list(input('Enter finite alphabet: '))
-finite_alphabet = list('*d%#')
-# not_encrypted = list(input('Enter not_encrypted string: '))
-not_encrypted = list('abacabadaba')
-# not_encrypted = list(input('Enter encrypted string: '))
-encrypted = list('#*%*d*%')
+original_alphabet = list(input('Enter original alphadet: '))
+# original_alphabet = list('abcd')
+finite_alphabet = list(input('Enter finite alphabet: '))
+# finite_alphabet = list('*d%#')
+not_encrypted = list(input('Enter not_encrypted string: '))
+# not_encrypted = list('abacabadaba')
+not_encrypted = list(input('Enter encrypted string: '))
+# encrypted = list('#*%*d*%')
+
 
 def encoder_decoder(original_alphabet, finite_alphabet):
     decoding_mask = dict(zip(original_alphabet, finite_alphabet))
     return decoding_mask
 
-def encoding(decoding_mask, not_encrypted):
+def sub_encoding(dm, not_encrypted):
     for i in not_encrypted:
-        if i in decoding_mask.keys():
-            print(decoding_mask.keys()[i], end='')
+        if i in dm.keys():
+            print(dm.get(i), end='')
 
+def sub_decoding(dm, encrypted):
+    for i in encrypted:
+        for key, value in dm.items():
+            if i == value:
+                print(key, end='')
+        
 
-    
-
-decoding_mask = encoder_decoder(original_alphabet, finite_alphabet)
-encoding(decoding_mask, not_encrypted)
- 
+dm = encoder_decoder(original_alphabet, finite_alphabet)
+sub_encoding(dm, not_encrypted)
+print()
+sub_decoding(dm, encrypted)
  
  
  
