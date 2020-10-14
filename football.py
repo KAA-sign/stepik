df284a4b435fc853cf896647a1c7661773754471
KAA-sign <q89308033587@gmail.com>
2020-10-14 16:44:55 +0300

    +

--- football.py
+++ football.py
@@ -1,17 +1,15 @@
-parameters = {'number_of_games': 0, 'victory': 0, 'drawn': 0 , 'defeat': 0, 'game_points': 0}
-
 def table_off_games(table_off_games = {}):
+    parameters = {'number_of_games': 0, 'victory': 0, 'drawn': 0 , 'defeat': 0, 'game_points': 0}
     teams = []
+    keys_tog = []
     table_off_games_interim = {}
     i = 0
     quantity = int(input('Еnter the number of games: '))
     while i < quantity:
-        # team_1, score_1, team_2, score_2  = ('Спартак;9;Зенит;10').split(';')
         team_1, score_1, team_2, score_2  = input('Enter team name and score: ').split(';')
         teams = [team_1, team_2]
-        for team in teams:
-            if team in table_off_games.keys():
-                teams.remove(team)      
+        keys_tog = list(table_off_games.keys())
+        teams = list(set(teams) - set(keys_tog)) 
         table_off_games_interim = {team: {key:value for key, value in parameters.items()} for team in teams}
         table_off_games.update(table_off_games_interim)
         if int(score_1) > int(score_2):
@@ -34,9 +32,17 @@ def table_off_games(table_off_games = {}):
             table_off_games[team_1]['drawn'] += 1
             table_off_games[team_2]['drawn'] += 1
         i += 1
-    print(table_off_games)
+    return table_off_games
+
+def print_tog(dict_tog):
+    for key, value in dict_tog.items():
+        print(key, ':', end=' ')
+        for key, value in value.items():
+            print(value, end=' ')
+        print()
+
+    
+
+       
 table_off_games()
-# print(table_off_team_set)
-# print(table_off_games)
-# for team, score in table_off_games.items():
-#     print((team+':'), *score, end='\n')
+print_tog(table_off_games)
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-parameters = {'number_of_games': 0, 'victory': 0, 'drawn': 0 , 'defeat': 0, 'game_points': 0}
-team_1, score_1, team_2, score_2  = ('Спартак;9;Зенит;10').split(';')
-team = [team_1, team_2]
-table_off_games = {team: {key:value for key, value in parameters.items()} for team in team}
-print(table_off_games)
+def print_tog():
+    for key in table_off_games.iterkeys():
+        print(key, end='')
+        for items in table_off_games[key]:
+            print(items, end='')
+            for values in table_off_games[key][items]:
+                print(values, end='')
+                
+
+table_off_games = {'Спартак': {'number_of_games': 2, 'victory': 0, 'drawn': 0, 'defeat': 2,     'game_points': 0},
+                    'Зенит': {'number_of_games': 2, 'victory': 1, 'drawn': 0, 'defeat': 1, 'game_points': 3}, 
+                    'Локомотив': {'number_of_games': 1, 'victory': 1, 'drawn': 0, 'defeat': 0, 'game_points': 3}}
